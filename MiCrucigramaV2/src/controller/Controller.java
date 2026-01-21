b231ad64c2255f9661f46719e63609ccb4d298b4
Nutellani <galleranimatias@gmail.com>
2026-01-21 19:44:31 -0300

    Iniciado un nuevo y espero que ULTIMO crucigrama :)

---
+++ MiCrucigramaV2/src/controller/Controller.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import model.Model;
+import view.View;
+import java.awt.event.ActionListener;
+
+public class Controller implements ActionListener{
+    
+    private Model model;
+    private View view;
+    
+    public Controller(Model model, View view){
+        
+    }
+
+    public void iniciar(){
+        
+    }    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ MiCrucigramaV2/src/main/main.java
@@ -0,0 +1,20 @@
+package main;
+
+import model.Model;
+import view.View;
+import controller.Controller;
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        Model model = new Model();
+        View view = new View();
+        
+        Controller controller = new Controller(model, view);
+        controller.iniciar();
+        view.setVisible(true);
+        
+    }
+    
+}
